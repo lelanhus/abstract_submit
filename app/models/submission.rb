366fcacd85810f75ff0de4e203a665d019a7f82c
Leland Husband <leland.husband@gmail.com>
2010-12-29 13:13:36 -0600

    modified tests for user model with submission.  removed users fixtures

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -1,4 +1,7 @@
 class Submission < ActiveRecord::Base
+
+  belongs_to :user
+  
   validates :title, :presence => true, :uniqueness => { :scope => :user_id }  
   validates :user_id, :presence => true
 end
--- test/factories.rb
+++ test/factories.rb
@@ -2,5 +2,11 @@
 
 Factory.define :submission do |f|
   f.sequence(:title) { |n| Faker::Lorem.sentence + "#{n}" }
-  f.sequence(:user_id) { |n| n }
+  f.association :user
+end
+
+Factory.define :user do |f|
+  f.sequence(:email) { |n| "joe-#{n}@example.com" }
+  f.sequence(:password) { |n| "password-#{n}" }
+  f.sequence(:password_confirmation) { |n| "password-#{n}" }
 end
