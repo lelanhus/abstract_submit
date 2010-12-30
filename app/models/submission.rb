14f01ac93cb5786a0fba628bac77b2a07dd8ddab
Leland Husband <leland.husband@gmail.com>
2010-12-30 13:47:13 -0600

    paper clip stuff

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -5,16 +5,19 @@ class Submission < ActiveRecord::Base
   has_many :authorships
   has_many :authors, :through => :authorships
   
-  has_attached_file :image
+  has_attached_file :image, :storage => :s3, 
+                    :s3_credentials => {
+                      :access_key_id => ENV['S3_KEY'],
+                      :secret_access_key => ENV['S3_SECRET']
+                    },
+                    :bucket => ENV['S3_BUCKET'],
+                    :path => "/submission_images/:filename"
+  
   validates :title, :presence => true, :uniqueness => { :scope => :user_id }  
   validates :user_id, :presence => true
   validates :body, :presence => true
   validates_attachment_size :image, :less_than => 4.megabytes
   validates_attachment_content_type :image, :content_type => ['image/jpeg', 'image/png']
   
-  attr_accessible :title, :body, :image_file_name
-  
-  def has_image?
-    self.image_file_name.nil?
-  end
+  attr_accessible :title, :body, :image
 end
