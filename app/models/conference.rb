1a2ef0da3ad2bfcaad5f3f8eac7e2a4d34521b83
Leland Husband <leland.husband@gmail.com>
2010-12-30 17:15:53 -0600

    added some scopes to conferences

--- app/models/conference.rb
+++ app/models/conference.rb
@@ -1,6 +1,14 @@
 class Conference < ActiveRecord::Base
   has_many :submissions
   
+  scope :current, where("conferences.end_date > ?", Date.today)
+  scope :past, where("conferences.end_date < ?", Date.today)
+  scope :with_open_submissions, where("conferences.submission_deadline > ?", Time.now)
+  
   validates :start_date, :end_date, :submission_deadline, :presence => true
   validates :name, :presence => true, :uniqueness => true
+  
+  def taking_submissions?
+    Time.now > self.submission_deadline
+  end
 end
