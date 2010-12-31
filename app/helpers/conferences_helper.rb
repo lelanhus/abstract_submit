782c224d7cb80a76ec12e193b7cd3b01b19cff16
Leland Husband <leland.husband@gmail.com>
2010-12-30 18:32:37 -0600

    changed up conferences index

--- app/helpers/conferences_helper.rb
+++ app/helpers/conferences_helper.rb
@@ -1,2 +1,8 @@
 module ConferencesHelper
+  
+  def submission_deadline_distance(conference)
+    return distance_of_time_in_words(Time.now, conference.submission_deadline) if 
+      conference.taking_submissions?
+    "No longer accepting submissions."
+  end
 end
